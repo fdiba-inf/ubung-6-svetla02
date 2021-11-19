b3054494f8f85e9e7939e74cc181fdc692a5a687
svetla02 <>
2021-11-19 09:16:00 +0000

    svet

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        
+        int reversedNumber = 0;
+        int manipulatedNumber = number;
+        while (manipulatedNumber > 0) {
+          int nextDigit = manipulatedNumber % 10;
+          reversedNumber = reversedNumber * 10 + nextDigit;
+          manipulatedNumber = manipulatedNumber / 10;
+        }
+        boolean palindrome = number == reversedNumber;
+        System.out.println("Palindrome: " + palindrome);
+
     }
 }
